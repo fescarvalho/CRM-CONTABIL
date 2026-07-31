23be900a1f6cb302c2785df6c6568ae0c1309aa8
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2026-07-31 11:53:37 -0300

    fix: remove earlyAccess do prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,6 @@ import 'dotenv/config'
 import { defineConfig, env } from '@prisma/config'
 
 export default defineConfig({
-  earlyAccess: true,
   datasource: {
     url: env('DATABASE_URL'),
   },
