5577fd09fdbdaf1db7da9e464138ce8064c072fd
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2026-08-04 11:15:36 -0300

    fix: bypass NextConfig TS error and apply both bodySizeLimits

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // @ts-ignore
   serverActions: {
     bodySizeLimit: '50mb'
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb'
+    }
   }
 };
 
